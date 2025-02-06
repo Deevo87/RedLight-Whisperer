62b5e40b31b5c85b16cb78b86efd34745d4940f7
rafal-laskowski <rafal.laskowski02@gmail.com>
2025-02-06 12:05:26 +0100

    define LaneGroup, LaneType and Light enums for traffic light control

---
+++ src/main/java/org/redlightwhisperer/enums/LaneGroup.java
@@ -0,0 +1,5 @@
+package org.redlightwhisperer.enums;
+
+public enum LaneGroup {
+    NS_FORWARD, EW_FORWARD, NS_TURN_LEFT, EW_TURN_LEFT, NS_TURN_RIGHT, EW_TURN_RIGHT
+}
---
+++ src/main/java/org/redlightwhisperer/enums/Light.java
@@ -0,0 +1,5 @@
+package org.redlightwhisperer.enums;
+
+public enum Light {
+    RED, GREEN, YELLOW
+}
