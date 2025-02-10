d7ba2e9f0014eb91ab11a1eacb65c64a45ff33c9
rafal-laskowski <rafal.laskowski02@gmail.com>
2025-02-10 18:29:03 +0100

    add helper methods to LaneType for prefix extraction and lane type checking

--- src/main/java/org/redlightwhisperer/enums/LaneType.java
+++ src/main/java/org/redlightwhisperer/enums/LaneType.java
@@ -3,5 +3,13 @@ package org.redlightwhisperer.enums;
 public enum LaneType {
     NORTH_FORWARD, SOUTH_FORWARD, EAST_FORWARD, WEST_FORWARD,
     NORTH_TURN_LEFT, SOUTH_TURN_LEFT, EAST_TURN_LEFT, WEST_TURN_LEFT,
-    NORTH_TURN_RIGHT, SOUTH_TURN_RIGHT, EAST_TURN_RIGHT, WEST_TURN_RIGHT
+    NORTH_TURN_RIGHT, SOUTH_TURN_RIGHT, EAST_TURN_RIGHT, WEST_TURN_RIGHT;
+
+    public String getPrefix() {
+        return this.name().substring(0, this.name().indexOf("_"));
+    }
+
+    public boolean isDoubleLine() {
+        return this == NORTH_FORWARD || this == SOUTH_FORWARD;
+    }
 }
