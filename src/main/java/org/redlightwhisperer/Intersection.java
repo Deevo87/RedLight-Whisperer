4d2161ae7c60349bee74719b10e8f0a3b09283de
rafal-laskowski <rafal.laskowski02@gmail.com>
2025-02-10 19:00:12 +0100

    add traffic light priority calculation, command handling and JavaFX controller integration

--- src/main/java/org/redlightwhisperer/Intersection.java
+++ src/main/java/org/redlightwhisperer/Intersection.java
@@ -1,52 +1,70 @@
 package org.redlightwhisperer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.redlightwhisperer.enums.*;
+import org.redlightwhisperer.gui.IntersectionController;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Intersection {
-    private final int RED_LIGHT_CHANGE_BUFFER = 2;
+    private final int RED_LIGHT_CHANGE_BUFFER = 5;
+    private final int TRAFFIC_USAGE_LIMIT = 8;
+    private final int PUNISHMENT = 5;
 
     private final CommandList commandList;
+    private final String outputFilename;
+    private final IntersectionController controller;
     private final StepStatuses stepStatuses = new StepStatuses();
-    private final Map<LaneGroup, List<TrafficLightLane>> trafficLaneGroupMap = new HashMap<>();
-    private final Map<LaneType, TrafficLightLane> lanes = new HashMap<>();
+    private final Map<LaneGroup, List<TrafficLane>> trafficLaneGroupMap = new HashMap<>();
+    private final Map<LaneType, TrafficLane> lanes = new HashMap<>();
     private final Map<LaneGroup, Integer> trafficGroupCnts = new HashMap<>();
     private LaneGroup previousFlowGroup = null;
     private LaneGroup biggestFlowGroup;
+    private int trafficUsage = 0;
+    private LaneGroup punishedLaneGroup;
+    private int punishmentCnt = PUNISHMENT;
+    private boolean mustChangeLight = false;
 
-    public Intersection(CommandList commandList) {
+    public Intersection(CommandList commandList, IntersectionController controller, String outputFilename) {
         this.commandList = commandList;
-
+        this.controller = controller;
+        this.outputFilename = outputFilename;
         init();
     }
 
     private void init() {
         for (LaneType laneType : LaneType.values()) {
-            lanes.put(laneType, new TrafficLightLane(laneType));
+            if (laneType.isDoubleLine()) {
+                lanes.put(laneType, new DoubleLaneTraffic(laneType, controller));
+            } else {
+                lanes.put(laneType, new SingleLaneTraffic(laneType, controller));
+            }
         }
 
-        addLaneGroup(LaneGroup.NS_FORWARD, LaneType.NORTH_FORWARD, LaneType.SOUTH_FORWARD);
-        addLaneGroup(LaneGroup.EW_FORWARD, LaneType.WEST_FORWARD, LaneType.EAST_FORWARD);
-        addLaneGroup(LaneGroup.NS_TURN_LEFT, LaneType.NORTH_TURN_LEFT, LaneType.SOUTH_TURN_LEFT);
-        addLaneGroup(LaneGroup.EW_TURN_LEFT, LaneType.WEST_TURN_LEFT, LaneType.EAST_TURN_LEFT);
-        addLaneGroup(LaneGroup.NS_TURN_RIGHT, LaneType.NORTH_TURN_RIGHT, LaneType.SOUTH_TURN_RIGHT);
-        addLaneGroup(LaneGroup.EW_TURN_RIGHT, LaneType.WEST_TURN_RIGHT, LaneType.EAST_TURN_RIGHT);
+        addLaneGroups();
 
         for (LaneGroup group : LaneGroup.values()) {
             trafficGroupCnts.put(group, 0);
         }
     }
 
+    private void addLaneGroups() {
+        addLaneGroup(LaneGroup.NS_FORWARD, LaneType.NORTH_FORWARD, LaneType.SOUTH_FORWARD, LaneType.EAST_TURN_RIGHT, LaneType.WEST_TURN_RIGHT);
+        addLaneGroup(LaneGroup.EW_FORWARD, LaneType.WEST_FORWARD, LaneType.EAST_FORWARD, LaneType.SOUTH_TURN_RIGHT, LaneType.NORTH_TURN_RIGHT);
+        addLaneGroup(LaneGroup.NS_TURN_LEFT, LaneType.NORTH_TURN_LEFT, LaneType.SOUTH_TURN_LEFT);
+        addLaneGroup(LaneGroup.EW_TURN_LEFT, LaneType.WEST_TURN_LEFT, LaneType.EAST_TURN_LEFT);
+    }
+
     private void addLaneGroup(LaneGroup laneGroup, LaneType... laneTypes) {
-        List<TrafficLightLane> trafficList = new ArrayList<>();
+        List<TrafficLane> trafficList = new ArrayList<>();
         for (LaneType laneType : laneTypes) {
             trafficList.add(lanes.get(laneType));
         }
-
         trafficLaneGroupMap.put(laneGroup, trafficList);
     }
 
@@ -57,76 +75,138 @@ public class Intersection {
             switch (command.getType()) {
                 case CommandType.ADD_VEHICLE -> {
                     i += 1;
-                    Car car = command.getCar();
-                    lanes.get(car.startRoad().getCarMovement(car.endRoad())).addCarToQueue(car);
+                    handleAddVehicle(command);
                 }
                 case CommandType.STEP -> {
-                    StepStatus currentStep = new StepStatus();
-                    calculatePriority();
+                    if (!calculatePriority()) {
+                        i += 1;
+                        stepStatuses.addStepStatus(new StepStatus());
+                        break;
+                    }
 
                     if (trafficLaneGroupMap.get(biggestFlowGroup).getFirst().getCurrentLight() == Light.GREEN) {
                         i += 1;
-                        for (TrafficLightLane lane : trafficLaneGroupMap.get(biggestFlowGroup)) {
-                            trafficGroupCnts.put(biggestFlowGroup, trafficGroupCnts.get(biggestFlowGroup) - 1);
-                            currentStep.addVehicle(lane.getExitedVehicles());
-                        }
-
-                        switch (biggestFlowGroup) {
-                            case NS_FORWARD -> {
-                                for (TrafficLightLane lane : trafficLaneGroupMap.get(LaneGroup.NS_TURN_RIGHT)) {
-                                    trafficGroupCnts.put(biggestFlowGroup, trafficGroupCnts.get(LaneGroup.NS_TURN_RIGHT) - 1);
-                                    currentStep.addVehicle(lane.getExitedVehicles());
-                                }
-                            }
-                            case EW_FORWARD -> {
-                                for (TrafficLightLane lane : trafficLaneGroupMap.get(LaneGroup.EW_TURN_RIGHT)) {
-                                    trafficGroupCnts.put(biggestFlowGroup, trafficGroupCnts.get(LaneGroup.EW_TURN_RIGHT) - 1);
-                                    currentStep.addVehicle(lane.getExitedVehicles());
-                                }
-                            }
-                        }
-
-                        previousFlowGroup = biggestFlowGroup;
-                        stepStatuses.addStepStatus(currentStep);
+                        handleGreenLight();
                     }
 
+                    createSimulationDelay(1000);
                 }
                 default -> {
                 }
             }
         }
+        saveStepStatuses();
     }
 
-    private void calculatePriority() {
-        int cnt;
-        int priority = -1;
-        for (Map.Entry<LaneGroup, List<TrafficLightLane>> laneGroup : trafficLaneGroupMap.entrySet()) {
-            for (TrafficLightLane traffic : laneGroup.getValue()) {
-                cnt = trafficGroupCnts.get(laneGroup.getKey()) + traffic.getQueueSize();
-                trafficGroupCnts.put(laneGroup.getKey(), cnt);
+    private void handleGreenLight() {
+        StepStatus currentStep = new StepStatus();
+        int cnt = trafficGroupCnts.get(biggestFlowGroup);
+        for (TrafficLane lane : trafficLaneGroupMap.get(biggestFlowGroup)) {
+            cnt -= 1;
+            currentStep.addVehicle(lane.getExitedVehicles());
+            createSimulationDelay(1000);
+        }
+        trafficGroupCnts.put(biggestFlowGroup, cnt);
 
-                if (biggestFlowGroup == laneGroup.getKey() && trafficLaneGroupMap.get(biggestFlowGroup).getFirst().getCurrentLight() == Light.GREEN) {
-                    cnt += RED_LIGHT_CHANGE_BUFFER;
-                }
-                if (cnt > priority) {
-                    priority = cnt;
-                    biggestFlowGroup = laneGroup.getKey();
-                }
+        previousFlowGroup = biggestFlowGroup;
+        stepStatuses.addStepStatus(currentStep);
+    }
+
+    private void handleAddVehicle(Command command) {
+        Vehicle vehicle = command.getVehicle();
+        lanes.get(vehicle.startRoad().getVehicleMovement(vehicle.endRoad())).addVehicleToQueue(vehicle);
+    }
+
+    private boolean calculatePriority() {
+        int priority = -1;
+        LaneGroup biggestFlowCandidate = biggestFlowGroup;
+
+        for (Map.Entry<LaneGroup, List<TrafficLane>> laneGroup : trafficLaneGroupMap.entrySet()) {
+            if (mustChangeLight && punishedLaneGroup == laneGroup.getKey()) {
+                punishmentCnt -= 1;
+                continue;
+            }
+
+            int cnt = 0;
+            for (TrafficLane traffic : laneGroup.getValue()) {
+                cnt += traffic.getQueueSize();
+            }
+            trafficGroupCnts.put(laneGroup.getKey(), cnt);
+
+            if (shouldPrioritizeCurrentGroup(laneGroup.getKey(), cnt)) {
+                cnt += RED_LIGHT_CHANGE_BUFFER;
+            }
+
+            if (cnt > priority) {
+                priority = cnt;
+                biggestFlowCandidate = laneGroup.getKey();
             }
         }
-        if (biggestFlowGroup == previousFlowGroup) {
-            return;
+        biggestFlowGroup = biggestFlowCandidate;
+
+        if (punishmentCnt <= 0) {
+            punishmentCnt = PUNISHMENT;
+            mustChangeLight = false;
         }
-        changeLightForGroup(biggestFlowGroup);
+
+        if (priority <= 0) {
+            return resetTrafficUsage();
+        }
+
+        manageTrafficLightChanges();
+        return true;
+    }
+
+    private boolean shouldPrioritizeCurrentGroup(LaneGroup laneGroup, int cnt) {
+        return biggestFlowGroup == laneGroup &&
+                trafficLaneGroupMap.get(biggestFlowGroup).getFirst().getCurrentLight() == Light.GREEN &&
+                cnt > 0;
+    }
+
+    private boolean resetTrafficUsage() {
+        if (mustChangeLight) {
+            biggestFlowGroup = punishedLaneGroup;
+            mustChangeLight = false;
+            trafficUsage = 0;
+            return true;
+        }
+        return false;
+    }
+
+    private void manageTrafficLightChanges() {
+        if (biggestFlowGroup == previousFlowGroup) {
+            trafficUsage += 1;
+            if (trafficUsage >= TRAFFIC_USAGE_LIMIT) {
+                trafficUsage = 0;
+                mustChangeLight = true;
+                punishedLaneGroup = biggestFlowGroup;
+            }
+        } else {
+            changeLightForGroup(biggestFlowGroup);
+            if (previousFlowGroup != null) {
+                changeLightForGroup(previousFlowGroup);
+            }
+        }
     }
 
     private void changeLightForGroup(LaneGroup group) {
-        for (TrafficLightLane lane : trafficLaneGroupMap.get(group)) {
-            lane.onLightChange();
+        trafficLaneGroupMap.get(group).forEach(TrafficLane::onLightChange);
+    }
+
+    private void saveStepStatuses() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            objectMapper.writeValue(new File(outputFilename), stepStatuses);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public StepStatuses getStepStatuses() {
-        return stepStatuses;
+    private void createSimulationDelay(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
