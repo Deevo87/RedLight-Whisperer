38e2487010f98a570bac3c7882b9cfb2372096cd
rafal-laskowski <rafal.laskowski02@gmail.com>
2025-02-10 19:01:36 +0100

    initialize JavaFX app, load commands from JSON, and setup UI

--- src/main/java/org/redlightwhisperer/Main.java
+++ src/main/java/org/redlightwhisperer/Main.java
@@ -2,31 +2,51 @@ package org.redlightwhisperer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import org.redlightwhisperer.gui.IntersectionController;
 
-import java.io.File;
 import java.io.IOException;
 
-public class Main {
+import java.io.File;
+import java.util.List;
+
+public class Main extends Application {
+
     public static void main(String[] args) {
+        System.out.println(args.length);
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        List<String> args = getParameters().getRaw();
+
+        for (String arg : args) {
+            System.out.println(arg);
+        }
         ObjectMapper objectMapper = new ObjectMapper();
         CommandList commandList;
         try {
-            commandList = objectMapper.readValue(new File("src/main/resources/test.json"), CommandList.class);
+            System.out.println(args.getFirst());
+            commandList = objectMapper.readValue(new File("src/main/resources/" + args.getFirst()), CommandList.class);
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Couldn't parse json, error: " + e);
         } catch (IOException e) {
             throw new RuntimeException("Couldn't open the file, error: " + e);
         }
 
-        Intersection intersection = new Intersection(commandList);
-        intersection.startSimulation();
-        StepStatuses stepStatuses = intersection.getStepStatuses();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main_view.fxml"));
+        Pane root = loader.load();
+        IntersectionController controller = loader.getController();
+        controller.setCommandListAndOutputFile(commandList, args.get(1));
 
-        try {
-            objectMapper.writeValue(new File("stepStatuses.json"), stepStatuses);
-        } catch (IOException e) {
-            throw new RuntimeException("Couldn't save result file, error: " + e);
-        }
-
+        Scene scene = new Scene(root, 800, 800);
+        primaryStage.setTitle("Red Light Whisperer Simulation");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
